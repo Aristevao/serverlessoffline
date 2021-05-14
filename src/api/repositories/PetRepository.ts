6adcb5371ac223a225f751ee02b8c07568f706d7
Lucas Ferreira dos Santos <contato.lferreira@hotmail.com>
2021-05-14 14:46:03 -0300

    [M-0003] add domain class and a new find_one_by_id route

--- src/api/repositories/PetRepository.ts
+++ src/api/repositories/PetRepository.ts
@@ -1,36 +1,22 @@
+import { Pet } from '../../models/Pet';
 import { species } from '../enums/species';
 
-const pet_entity_database = {
-    "one": {
-        id: 1,
-        name: 'Lily',
-        age: 10,
-        species: species.CAT,
-    },
-    "two":{
-        id: 2,
-        name: 'Gato',
-        age: 8,
-        species: species.DOG,
-    },
-    "three":{
-        id: 3,
-        name: 'Suzi',
-        age: 20,
-        species: species.BIRD,
-    }
-};
+const pet_entity_database = [
+    new Pet(1, 'Lily', 10, species.CAT),
+    new Pet(2, 'Joca', 8, species.DOG),
+    new Pet(3, 'Suzi', 20, species.BIRD),
+];
 
 export class PetRepository {
     public insert(): any {
         console.log('Error, method not implemented');
     }
     
-    public findOneById(): any {
-        return pet_entity_database.one;
+    public findOneById(): Pet {
+        return pet_entity_database[0];
     }
     
-    public findMany(): any {
+    public findMany(): Pet[] {
         return pet_entity_database;
     }
     
