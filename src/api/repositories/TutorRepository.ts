4cb8e804fdd65ae1b297513f40cc57bd8cf14b96
Aristevao <68197280+Aristevao@users.noreply.github.com>
2021-05-24 14:29:38 -0300

    [M-0005] Added CRUD for Tutor entity (#7)
    Implementations:
    - Created new entity, Tutor;
    - Implemented CRUD for Tutor entity.
    
    * Added CRUD for Tutor entity
    * Added missing types and refactored remove function

---
+++ src/api/repositories/TutorRepository.ts
@@ -0,0 +1,36 @@
+import { Tutor } from '../../models/Tutor';
+
+var tutor_entity_database = [
+    new Tutor(1, 'Ana', 'Silva', 21),
+    new Tutor(2, 'Beatriz', 'Santos', 22),
+    new Tutor(3, 'Carlos', 'Oliveira', 23),
+];
+
+export class TutorRepository {
+    public insert(tutor: Tutor): Tutor {
+        tutor.id = tutor_entity_database.length + 1;
+        tutor_entity_database.push(tutor);
+        return tutor;
+    }
+
+    public findOneById(id: number): Tutor {
+        return tutor_entity_database[id];
+    }
+
+    public findMany(): Tutor[] {
+        return tutor_entity_database;
+    }
+
+    public update(id: number, tutor: Tutor): Tutor {
+        tutor_entity_database[id] = tutor;
+        tutor.id = id;
+        return tutor;
+    }
+
+    public remove(id: number): Tutor[] {
+        tutor_entity_database = tutor_entity_database.filter(
+            (entry) => { if (entry.id != id) return entry }
+        );
+        return tutor_entity_database;
+    }
+}
