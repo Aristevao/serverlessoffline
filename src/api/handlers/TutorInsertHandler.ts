4192a9dd8cfe02217dfc8d92be663e73b082ac67
Aristevao <68197280+Aristevao@users.noreply.github.com>
2021-05-29 16:26:53 -0300

    [M-0005] Add crud for Tutor entity (#9)
    * Added CRUD for Tutor entity
    
    * Added missing types and refactored remove function
    
    * Changing tutor_entity_database 'var' to 'let'

---
+++ src/api/handlers/TutorInsertHandler.ts
@@ -0,0 +1,24 @@
+import { Handler } from "aws-lambda";
+import { TutorService } from '../services/TutorService';
+
+class TutorInsertHandler {
+    private service: TutorService;
+
+    constructor() {
+        this.service = new TutorService();
+    }
+
+    public execute(event: any) {
+        const tutors = this.service.insert(JSON.parse(event.body));        
+        const response = {
+            statusCode: 201,
+            body: JSON.stringify(tutors),
+        };
+        return response;
+    }
+}
+
+export const handler: Handler = (event, context, callback) => {
+    const response = new TutorInsertHandler().execute(event);
+    callback(null, response);
+}
