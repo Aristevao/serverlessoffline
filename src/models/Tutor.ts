50f1341ec6185388adde3f267e8c5ad94b7c2cce
Aristevao <aristevao@hotmail.com>
2021-05-30 19:33:32 -0300

    Added TypeORM decorators for Tutor model

--- src/models/Tutor.ts
+++ src/models/Tutor.ts
@@ -1,7 +1,18 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Tutor {
+
+    @PrimaryGeneratedColumn()
     public id: number
+
+    @Column()
     public name: string
+
+    @Column()
     public lastName: string
+
+    @Column()
     public age: number
 
     constructor(
